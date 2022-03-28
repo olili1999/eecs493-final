db2afa30d2d969998bc426a80efa2057f5385900
olili1999 <olili@umich.edu>
2022-03-28 12:14:33 -0400

    remove commented env stuff

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -19,26 +19,6 @@ const firebaseConfig = {
 
 
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import {getFirestore} from '@firebase/firestore';
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDkgpulYTt_ZMRQ9zRUq5BJX3fCAp3bagA",
-//   authDomain: "eecs493-final-project-a6184.firebaseapp.com",
-//   projectId: "eecs493-final-project-a6184",
-//   storageBucket: "eecs493-final-project-a6184.appspot.com",
-//   messagingSenderId: "596949176240",
-//   appId: "1:596949176240:web:c1eefa95ce519131995075",
-//   measurementId: "G-VMPEKZ8MW0"
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
